983cb4342947a5c9f702bb9b32d8a9f9014be338
Simon Christmann <simon@christmann.email>
2021-01-02 15:22:41 +0100

    fix config

--- index.js
+++ index.js
@@ -7,9 +7,9 @@ const config = require('yargs')
     .usage(pkg.name + ' ' + pkg.version + '\n' + pkg.description + '\n\nUsage: $0 [options]')
     .describe('verbosity', 'possible values: "error", "warn", "info", "debug"')
     .describe('mqtt-url', 'mqtt broker url. See https://github.com/mqttjs/MQTT.js#connect-using-a-url')
-    .describe('influx-host')
-    .describe('influx-port')
-    .describe('influx-database')
+    .describe('influxdb-host')
+    .describe('influxdb-port')
+    .describe('influxdb-database')
     .describe('subscription', 'array of topics to subscribe').array('subscription')
     .alias({
         h: 'help',
@@ -18,9 +18,9 @@ const config = require('yargs')
     })
     .default({
         'mqtt-url': 'mqtt://127.0.0.1',
-        'influx-host': '127.0.0.1',
-        'influx-port': 8086,
-        'influx-database': 'mqtt',
+        'influxdb-host': '127.0.0.1',
+        'influxdb-port': 8086,
+        'influxdb-database': 'mqtt',
         subscription: [
             '#'
         ]
@@ -37,9 +37,9 @@ log.info(pkg.name + ' ' + pkg.version + ' starting');
 log.debug('loaded config: ', config);
 
 const influx = new Influx.InfluxDB({
-    host: config.influxHost,
-    port: config.influxPort,
-    database: config.influxDatabase
+    host: config.influxdbHost,
+    port: config.influxdbPort,
+    database: config.influxdbDatabase
 });
 
 log.info('mqtt trying to connect', config.mqttUrl);
